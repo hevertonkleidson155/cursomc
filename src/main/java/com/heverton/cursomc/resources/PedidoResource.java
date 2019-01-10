d15e27c9c772a8e9d3e0b80ec55a8c1a192c44f1
Heverton Kleidson <hevertonkleidson155@gmail.com>
2019-01-10 18:07:09 -0300

    Criado tudo e mapeado

---
+++ src/main/java/com/heverton/cursomc/resources/PedidoResource.java
@@ -0,0 +1,27 @@
+package com.heverton.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.heverton.cursomc.domain.Pedido;
+import com.heverton.cursomc.services.PedidoService;
+
+@RestController
+@RequestMapping(value="/pedidos")//Quando requisitarem uma /categorias
+public class PedidoResource {
+	
+	//Cria uma objeto de PedidoService que e responsavel pelos comandos "SQL" 
+	@Autowired
+	private PedidoService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)//Pega o id passado pelo /{id}
+	public ResponseEntity<?> find(@PathVariable Integer id) {//Responde uma entidade qualquer funcao find(poderia ser outro nome) e usa o id que recebeu via get como parâmetro
+		
+		Pedido obj = service.buscar(id);//Cria um objeto com a resposta da função buscar contida em objeto de PedidoServices pasasndo o id que recebeu de parâmetro
+		return ResponseEntity.ok().body(obj);//Resposta ok e no corpo  responde o objeto 
+	}
+}
---
+++ src/main/java/com/heverton/cursomc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.heverton.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.heverton.cursomc.domain.Pedido;
+import com.heverton.cursomc.repositories.PedidoRepository;
+import com.heverton.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	//CRIA OS METODOS USANDO O REPOSITORIO QUE E QUEM FAZ O CRUD
+	@Autowired
+	private PedidoRepository repo; //Cria um objeto de repositorio
+	
+	public Pedido buscar(Integer id) {
+		Pedido obj = repo.findOne(id);//Usa o obj de repositorio para ir buscar no banco o id
+		if(obj==null){
+			throw new ObjectNotFoundException("NOT_FOUND");
+		}
+		return obj;
+	}
+	
+	
+}
