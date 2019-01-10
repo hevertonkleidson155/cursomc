7bbbfd5a0187bc1fe5f55ed14ee802e173a9958c
heverton155 <heverton155@users.noreply.github.com>
2019-01-09 21:28:42 -0300

    criando classe cliente

---
+++ src/main/java/com/heverton/cursomc/enums/TipoCliente.java
@@ -0,0 +1,37 @@
+package com.heverton.cursomc.enums;
+
+import org.dom4j.IllegalAddException;
+
+public enum TipoCliente {
+	PESSOAFISICA(1,"Pessoa Física"),
+	JURIDICA(2, "Pessoa Jurídica");
+	
+	private int cod;
+	private String descricao;
+	
+	private TipoCliente(int cod, String descricao) {
+		this.cod = cod;
+		this.descricao = descricao;
+	}
+	
+	public int getCod() {
+		return cod;
+	}
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public static TipoCliente toEnum(Integer cod) {
+		if(cod==null) {
+			return null;
+		}
+		for(TipoCliente x : TipoCliente.values()) {
+			if(cod.equals(x.getCod())) {
+				return x;
+			}
+			
+		}
+		throw new IllegalAddException("Id inválido: "+cod);
+	}
+}
