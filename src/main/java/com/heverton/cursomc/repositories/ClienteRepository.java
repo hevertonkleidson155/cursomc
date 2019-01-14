1ae2112585463935464d10535822047339e14676
Heverton Kleidson <hevertonkleidson155@gmail.com>
2019-01-14 09:32:49 -0300

    Busca por email e validacao se o email já foi cadastrado

--- src/main/java/com/heverton/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/heverton/cursomc/repositories/ClienteRepository.java
@@ -2,11 +2,15 @@ package com.heverton.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.heverton.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 	//Fazer CRUD da tabela CATEGORIA
+	
+	@Transactional(readOnly=true) //Faz com que seja só um read
+	Cliente findByEmail(String email); //Só com isso o spring identifica que é um select por email sem precisar montar nada
 	
 }
--- src/main/java/com/heverton/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/heverton/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,21 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.apache.tomcat.jni.File;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.heverton.cursomc.domain.Cliente;
 import com.heverton.cursomc.dto.ClienteNewDTO;
 import com.heverton.cursomc.enums.TipoCliente;
+import com.heverton.cursomc.repositories.ClienteRepository;
 import com.heverton.cursomc.resources.exceptions.FieldMessage;
 import com.heverton.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -27,7 +36,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if(objDto.getTipo().equals(TipoCliente.JURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj","CNPJ inválido"));
 	}
-			
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux!=null) {
+			list.add(new FieldMessage("email", "Email já cadastrado"));
+		}
 		// inclua os testes aqui, inserindo erros na lista
 
 		for (FieldMessage e : list) {
