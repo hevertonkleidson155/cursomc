79814b651c95761ed9b357ccbcf55f44c4287f47
Heverton Kleidson <hevertonkleidson155@gmail.com>
2019-01-14 17:59:52 -0300

    criando tables e inserindo dados igual ao h2

--- src/main/java/com/heverton/cursomc/config/DevConfig.java
+++ src/main/java/com/heverton/cursomc/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.heverton.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -10,14 +11,22 @@ import org.springframework.context.annotation.Profile;
 import com.heverton.cursomc.services.DBService;
 
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class DevConfig {
 	
 	@Autowired
 	private DBService dbService;
 	
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy)){
+			return false;
+		}
 		dbService.instantiatedTestDatabase();
 		return true;
 	}
