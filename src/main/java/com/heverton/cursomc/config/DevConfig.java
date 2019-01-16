206ca0fcd6f92b6f1aced962e149690e956e5816
Heverton Kleidson <hevertonkleidson155@gmail.com>
2019-01-16 15:14:50 -0300

    enviando email pelo google

--- src/main/java/com/heverton/cursomc/config/DevConfig.java
+++ src/main/java/com/heverton/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.heverton.cursomc.services.DBService;
+import com.heverton.cursomc.services.EmailService;
+import com.heverton.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,5 +31,10 @@ public class DevConfig {
 		}
 		dbService.instantiatedTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 }
